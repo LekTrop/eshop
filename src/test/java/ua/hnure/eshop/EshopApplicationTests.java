0f47014cb1c6b3768fde19cc838977f40b6c92dd
ozhytari <Oleksandr.Zhytariuk@Tideworks.com>
2022-08-19 16:57:41 +0300

    initial commit

---
+++ src/test/java/ua/hnure/eshop/EshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.hnure.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
